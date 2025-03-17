21944899dc2aaa9acb7c94771336ca927c83adc7
markopaunovic1 <marpau970514@hotmail.com>
2025-03-17 13:46:20 +0100

    working on hangmanStatus to display each image

--- src/components/HangmanStatus/HangmanStatus.jsx
+++ src/components/HangmanStatus/HangmanStatus.jsx
@@ -1,9 +1,26 @@
-import React from "react"
+import React, { useState } from "react";
+import images from "../../images";
 
 function HangmanStatus() {
-    return (
-        <h1>HangmanStatus section</h1>
-    )
+  const [hangmanImages, setHangmanImages] = useState([]);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const showNextImage = () => {
+    setCurrentImageIndex((prevIndex) => prevIndex + 1);
+  };
+
+  console.log(currentImageIndex);
+
+  return (
+    <section>
+      <img
+        key={currentImageIndex}
+        src={images[currentImageIndex]}
+        alt="hangman"
+      ></img>
+      <button onClick={showNextImage}>click for roll image</button>
+    </section>
+  );
 }
 
 export default HangmanStatus;
---
+++ src/images.js
@@ -0,0 +1,21 @@
+import hangmanImageDark1 from "../src/assets/hangman-images/dark-mode/hangman-1.png";
+import hangmanImageDark2 from "../src/assets/hangman-images/dark-mode/hangman-2-dark.png";
+import hangmanImageDark3 from "../src/assets/hangman-images/dark-mode/hangman-3-dark.png";
+import hangmanImageDark4 from "../src/assets/hangman-images/dark-mode/hangman-4-dark.png";
+import hangmanImageDark5 from "../src/assets/hangman-images/dark-mode/hangman-5-dark.png";
+import hangmanImageDark6 from "../src/assets/hangman-images/dark-mode/hangman-6-dark.png";
+import hangmanImageDark7 from "../src/assets/hangman-images/dark-mode/hangman-7-dark.png";
+import hangmanImageDark8 from "../src/assets/hangman-images/dark-mode/hangman-8-dark.png";
+
+const hangmanImagesDark = [
+  hangmanImageDark1,
+  hangmanImageDark2,
+  hangmanImageDark3,
+  hangmanImageDark4,
+  hangmanImageDark5,
+  hangmanImageDark6,
+  hangmanImageDark7,
+  hangmanImageDark8,
+];
+
+export default hangmanImagesDark;
